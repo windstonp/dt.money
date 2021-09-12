06150f33bad368b2dae2fbefe3bf79b9a127dd7d
windstonp <joaor4e@gmail.com>
2021-09-12 18:21:18 -0300

    DT-02 install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <Title>HELLO WORLD</Title>
     </div>
   );
 }
